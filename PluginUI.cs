3734d765506fe681b56bf32dd6fc675fe0a43285
status102 <3122262428@qq.com>
2022-06-08 21:48:04 +0800

    交易监控界面

--- PluginUI.cs
+++ PluginUI.cs
@@ -38,8 +38,9 @@ namespace TradeBuddy
 			set { this.tradeVisible = value; }
 		}
 
-		//保存单次交易时，监控窗口是否显示
-		public bool tradeOnceVisible = true;
+		public bool tradeOnceVisible = true;//保存单次交易时，监控窗口是否显示
+
+		public bool finalCheck = false;//在双方都确认的情况下进入最终交易确认
 
 		// passing in the image here just for simplicity
 		public PluginUI(Configuration configuration, ImGuiScene.TextureWrap goatImage)
@@ -123,38 +124,32 @@ namespace TradeBuddy
 			var tradeAddess = Plugin.plugin.GameGui.GetAddonByName("Trade", 1);
 			if (tradeAddess == IntPtr.Zero)
 			{
-				tradeOnceVisible = true;
+				tradeOnceVisible = true;//交易窗口关闭后重置单次关闭设置
+				finalCheck = false;//最终确认时取消
 				return;
 			}
 			if (!tradeOnceVisible) return;
 			var trade = (AtkUnitBase*)tradeAddess;
 			if (trade->UldManager.NodeListCount <= 0) return;
-			if (trade->UldManager.LoadedState != 3) return;
+			if (trade->UldManager.LoadedState != 3) return;//等待交易窗口加载完毕
 
-			int width = 350;
+			int width = 480;
 			ImGui.SetNextWindowSize(new Vector2(width, 600), ImGuiCond.Appearing);
 			ImGui.SetNextWindowPos(new Vector2(trade->X - width, trade->Y), ImGuiCond.Appearing);
-			if (ImGui.Begin("测试窗口", ref this.tradeOnceVisible/*, ImGuiWindowFlags.NoCollapse*/))
+			if (ImGui.Begin("测试窗口", ref this.tradeOnceVisible))
 			{
-
+				/*
 				var justhaveatry = this.configuration.ShowTrade;
 				if (ImGui.Checkbox("Checkbox", ref justhaveatry))
 				{
 					this.configuration.ShowTrade = justhaveatry;
 				}
-
+				*/
 				string[] header = new string[] { "物品", "数量", "预期金额" };
-				//trade->CollisionNodeList[0]->CollisionType
 				var receiveTarget = trade->UldManager.NodeList[20]->GetAsAtkTextNode()->NodeText;
 				var receiveMoney = trade->UldManager.NodeList[6]->GetAsAtkTextNode();
 				var receiveChecked = trade->UldManager.NodeList[31]->GetAsAtkComponentNode()->Component->UldManager.NodeList[0]->GetAsAtkImageNode()->AtkResNode.ScaleY == 1;
-				/*
-				var receiveItem1 = (AtkComponentIcon*)trade->UldManager.NodeList[19]->GetComponent();
-				var receiveItem2 = (AtkComponentIcon*)trade->UldManager.NodeList[18]->GetComponent();
-				var receiveItem3 = (AtkComponentIcon*)trade->UldManager.NodeList[17]->GetComponent();
-				var receiveItem4 = (AtkComponentIcon*)trade->UldManager.NodeList[16]->GetComponent();
-				var receiveItem5 = (AtkComponentIcon*)trade->UldManager.NodeList[15]->GetComponent();
-				*/
+
 				AtkResNode*[] receiveArray = new AtkResNode*[]
 				{
 					trade->UldManager.NodeList[19],
@@ -170,12 +165,6 @@ namespace TradeBuddy
 				//var giveItem1Pic = trade->UldManager.NodeList[30]->GetAsAtkComponentNode()->Component->UldManager.NodeList[2]->GetAsAtkComponentNode()->Component->UldManager.NodeList[0]->GetAsAtkImageNode();
 				var giveChecked = trade->UldManager.NodeList[32]->GetAsAtkComponentNode()->Component->UldManager.NodeList[0]->GetAsAtkImageNode()->AtkResNode.ScaleY == 1;
 
-				var giveItem1 = (AtkComponentIcon*)trade->UldManager.NodeList[30]->GetAsAtkComponentNode()->Component->UldManager.NodeList[2]->GetComponent();
-				var giveItem2 = (AtkComponentIcon*)trade->UldManager.NodeList[29]->GetAsAtkComponentNode()->Component->UldManager.NodeList[2]->GetComponent();
-				var giveItem3 = (AtkComponentIcon*)trade->UldManager.NodeList[28]->GetAsAtkComponentNode()->Component->UldManager.NodeList[2]->GetComponent();
-				var giveItem4 = (AtkComponentIcon*)trade->UldManager.NodeList[27]->GetAsAtkComponentNode()->Component->UldManager.NodeList[2]->GetComponent();
-				var giveItem5 = (AtkComponentIcon*)trade->UldManager.NodeList[26]->GetAsAtkComponentNode()->Component->UldManager.NodeList[2]->GetComponent();
-
 				AtkResNode*[] giveArray = new AtkResNode*[]
 				{
 					trade->UldManager.NodeList[30]->GetAsAtkComponentNode()->Component->UldManager.NodeList[2],
@@ -185,53 +174,71 @@ namespace TradeBuddy
 					trade->UldManager.NodeList[26]->GetAsAtkComponentNode()->Component->UldManager.NodeList[2]
 				};
 
-				Item? item1;
-
 				if (giveChecked)
 					ImGui.Text("支付方：√");
 				else
 					ImGui.Text("支付方：");
 
 
-				DrowNewTextRow("支付", 3, header, giveArray);
+				DrowTradeTable("支付", 3, new float[] { -1, 150, 300 }, header, giveArray, giveMoney);
 				ImGui.Text("支付：" + giveMoney->NodeText.ToString() + "p");
-
+				ImGui.Spacing();
 				if (receiveChecked)
 					ImGui.Text("接收方<" + receiveTarget + ">：√");
 				else
 					ImGui.Text("接收方<" + receiveTarget + ">：");
 
-				//DrowNewTextRow("接收", 3, header, receiveArray);
+				DrowTradeTable("接收", 3, new float[] { -1, 150, 300 }, header, receiveArray, receiveMoney);
 
 				ImGui.Text("接收：" + receiveMoney->NodeText.ToString() + "p");
 
+				if(!finalCheck &&receiveChecked && giveChecked)//双方交易确认，进入最终确认状态
+				{
+					var selectYesno = Plugin.plugin.GameGui.GetAddonByName("SelectYesno", 1);
+					if (selectYesno != IntPtr.Zero && ((AtkUnitBase*)selectYesno)->UldManager.LoadedState == 3)
+					{
+						var yesButton = ((AtkUnitBase*)selectYesno)->UldManager.NodeList[11];
+						var noButton = ((AtkUnitBase*)selectYesno)->UldManager.NodeList[8];
+						//var noButton = ((AtkUnitBase*)selectYesno)->UldManager.NodeList[8];
+						yesButton->AddEvent(AtkEventType.MouseClick, ,)
+						finalCheck = true;
+					}
+				}
 			}
 			ImGui.End();
 		}
 
-		//向正在从创建的表中追加
-		private unsafe void DrowNewTextRow(String title, int col, String[] headerText, AtkResNode*[] atkResNodeList)
+		//绘制交易物品表格
+		private unsafe void DrowTradeTable(String title, int col, float[] width, String[] headerText, AtkResNode*[] atkResNodeList, AtkTextNode* textNode)
 		{
 			AtkResNode* resNode;
 			long iconId;
 			int count;
 			string iconIdStr, itemName, countStr;
 
+			// todo 加入物品图片
+			//绘制5个道具栏
 			ImGui.BeginTable(title, col, ImGuiTableFlags.Resizable | ImGuiTableFlags.RowBg | ImGuiTableFlags.BordersInnerH);
 
 			for (int i = 0; i < col; i++)
 			{
 				ImGui.TableNextColumn();
 				ImGui.TableHeader(headerText[i]);
+
+				// todo 最好能让拉伸的时候只拉伸第一列
+				if (width.Length > i && width[i] >= 0)
+					ImGui.TableSetupColumn(headerText[i], ImGuiTableColumnFlags.WidthMask, width[i]);
+				else
+					ImGui.TableSetupColumn(headerText[i]);
 			}
-			//ImGui.SetColumnWidth(1, (float)15.0);
-			//ImGui.SetColumnWidth(2, (float)25.0);
 
 			for (int i = 0; i < atkResNodeList.Length; i++)
 			{
 				resNode = atkResNodeList[i];
 				iconId = ((AtkComponentIcon*)resNode->GetComponent())->IconId;
-				if (iconId < 1)
+
+				//放入交易格子中再取消时，iconId会残留为之前的数值
+				if (!resNode->IsVisible || iconId < 1)
 				{
 					ImGui.TableNextColumn();
 					ImGui.Text("");
@@ -244,11 +251,9 @@ namespace TradeBuddy
 				iconIdStr = Convert.ToString(iconId);
 
 				ImGui.TableNextColumn();
-				//ImGui.Text();
 
 				if (iconIdStr.StartsWith("10"))//HQ物品
 					iconId = Convert.ToInt64(iconIdStr[2..]);
-
 
 				var item1 = Plugin.plugin.DataManager.GetExcelSheet<Item>()?.FirstOrDefault(r => r.Icon == iconId);
 				//item1 = Plugin.plugin.DataManager.GetExcelSheet<Item>()?.FirstOrDefault(r => r.Icon == 10001);
@@ -270,13 +275,13 @@ namespace TradeBuddy
 				ImGui.TableNextColumn();
 				ImGui.Text(countStr);
 
-
+				// todo 计算预期金额
 				ImGui.TableNextColumn();
 				ImGui.Text("---");
-
 			}
 
 			ImGui.EndTable();
+
 		}
 	}
 }
