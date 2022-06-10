70310ac910dd29738a4ca065bd256a8e31957f09
status102 <3122262428@qq.com>
2022-06-10 12:27:21 +0800

    添加复制功能

---
+++ Window/HistoryUI.cs
@@ -0,0 +1,22 @@
+﻿using ImGuiNET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeBuddy.Window
+{
+	public class HistoryUI
+	{
+		public static void DrawHistory(ref bool historyVisible)
+		{
+			ImGui.SetNextWindowSize(new Vector2(232, 75), ImGuiCond.FirstUseEver);
+			if(ImGui.Begin("交易历史", ref historyVisible))
+			{
+
+			}
+		}
+	}
+}
